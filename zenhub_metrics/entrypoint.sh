fd876887998cece416d6b179b35cb1c8aa579ee4
Ringo De Smet <ringo@de-smet.name>
2017-12-15 15:21:02 +0100

    Updating ENDPOINT to allow running Django manage.py script as well as celery for background jobs

--- zenhub_metrics/entrypoint.sh
+++ zenhub_metrics/entrypoint.sh
@@ -4,10 +4,5 @@ set -e
 
 cd /opt/zenhub-charts
 ./manage.py migrate
-# automate mapping unknown pipelines to Closed
-sed -i -e "s/int(input('Select one: '))/5/" boards/fetcher/fetch.py 
-echo $REPOS_LIST | ./manage.py fetch --initial 
-celery -A zenhub_charts worker -B -l info &> celery.log &
-./manage.py runserver 0.0.0.0:8080
 
-exec "$@"
+"$@"
