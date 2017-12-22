882d460b9971308c6ba42e9bbcd959d4bfa0a1b5
iuri aranda <iuri.aranda@gmail.com>
2017-12-22 17:03:10 +0100

    Refactor env variables in teleport image & fix entrypoint

--- teleport/entrypoint.sh
+++ teleport/entrypoint.sh
@@ -15,30 +15,6 @@ if [ -z "${AUTH_SERVERS}" ] && [ "${ENABLE_PROXY}" == "yes" ]; then
   exit 1
 fi
 
-if [ -z "${DYNAMODB_REGION}" ]; then
-  export DYNAMODB_REGION=eu-west-1
-fi
-
-if [ -z "${ENABLE_AUTH}" ]; then
-  export ENABLE_AUTH=yes
-fi
-
-if [ -z "${ENABLE_PROXY}" ]; then
-  export ENABLE_PROXY=yes
-fi
-
-if [ -z "${DATA_DIR}" ]; then
-  export DATA_DIR="/var/lib/teleport"
-fi
-
-if [ -z "${LOG_OUTPUT}" ]; then
-  export LOG_OUTPUT="stdout"
-fi
-
-if [ -z "${LOG_SEVERITY}" ]; then
-  export LOG_SEVERITY="ERROR"
-fi
-
 TOKENS_=""
 if [ ! -z "${TOKENS}" ]; then
   TOKENS_="tokens:
