c0bf6b4198d46a9193bfeaabbc6d8e66bdb2b242
iuri aranda <iuri.aranda@gmail.com>
2017-12-22 09:44:17 +0100

    Use && instead of -a in bash

--- teleport/entrypoint.sh
+++ teleport/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ -z "${CLUSTER_NAME}" -a "${ENABLE_AUTH}" == "yes" ]; then
+if [ -z "${CLUSTER_NAME}" ] && [ "${ENABLE_AUTH}" == "yes" ]; then
   echo 'ERROR please fill in the $CLUSTER_NAME environment variable'
   exit 1
 fi
 
-if [ -z "${DYNAMODB_TABLE}" -a "${ENABLE_AUTH}" == "yes" ]; then
+if [ -z "${DYNAMODB_TABLE}" ] && [ "${ENABLE_AUTH}" == "yes" ]; then
   echo 'ERROR please fill in the $DYNAMODB_TABLE environment variable'
   exit 1
 fi
 
-if [ -z "${AUTH_SERVERS}" -a "${ENABLE_PROXY}" == "yes" ]; then
+if [ -z "${AUTH_SERVERS}" ] && [ "${ENABLE_PROXY}" == "yes" ]; then
   echo 'ERROR please provide at least one auth server in the $AUTH_SERVERS environment variable'
   exit 1
 fi
