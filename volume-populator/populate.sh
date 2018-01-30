562d2557ee7e4ce4184d980f8f5ae7700f8626b7
Sam Clinckspoor <sam.clinckspoor@gmail.com>
2018-01-30 10:59:43 +0100

    add more logging

--- volume-populator/populate.sh
+++ volume-populator/populate.sh
@@ -2,6 +2,14 @@
 
 export FILES_A=$(find $CONFIGMAP_DIR -maxdepth 1 -mindepth 1 -type f -exec basename {} \;)
 
+echo "contents of $CONFIGMAP_DIR":
+ls $CONFIGMAP_DIR
+echo ""
+echo "contents of $VOLUME_DIR":
+ls $VOLUME_DIR
+
+echo "starting to populate $VOLUME_DIR from $CONFIGMAP_DIR"
+
 for file in $FILES_A; do
 	if [ -f $VOLUME_DIR/$file ]; then
 		echo "$file exists in volume"
@@ -11,3 +19,5 @@ for file in $FILES_A; do
 		cp $CONFIGMAP_DIR/$file $VOLUME_DIR/$file
 	fi
 done
+
+echo "finished populating $VOLUME_DIR from $CONFIGMAP_DIR"
