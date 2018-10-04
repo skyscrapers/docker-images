4af612777bc9b7a2982b7c01c700cf7e8cb38cf2
iuri aranda <iuri.aranda@gmail.com>
2018-10-04 18:50:36 +0200

    Fix vault login command in concourse image

--- concourse/entrypoint.sh
+++ concourse/entrypoint.sh
@@ -7,7 +7,7 @@ if [ ! -z ${_CONCOURSE_KEYS_S3} ]; then aws s3 cp --recursive ${_CONCOURSE_KEYS_
 ## To be removed when concourse fully supports the AWS auth backend
 ## https://github.com/concourse/concourse/issues/1367
 if [ ! -z ${CONCOURSE_VAULT_URL} ] && [ "${CONCOURSE_VAULT_AUTH_BACKEND}" == "aws" ]; then
-  export CONCOURSE_VAULT_CLIENT_TOKEN="$(VAULT_ADDR=${CONCOURSE_VAULT_URL} /bin/vault auth -token-only -method=aws `echo -n ${CONCOURSE_VAULT_AUTH_PARAM} | tr ',' ' '`)"
+  export CONCOURSE_VAULT_CLIENT_TOKEN="$(VAULT_ADDR=${CONCOURSE_VAULT_URL} /bin/vault login -token-only -method=aws `echo -n ${CONCOURSE_VAULT_AUTH_PARAM} | tr ',' ' '`)"
   unset CONCOURSE_VAULT_AUTH_BACKEND
   unset CONCOURSE_VAULT_AUTH_PARAM
 fi
